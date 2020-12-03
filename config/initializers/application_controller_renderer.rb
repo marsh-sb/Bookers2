5219f4d14a1f2aa02608ffe46a78f3dea90da2fb
marsh-sb <enotaku5526@gmail.com>
2020-12-03 18:47:14 +0000

    [Add]ログイン.投稿.編集

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+
+Refile.secret_key = 'ffb18536ca1aab5b2e4dd4c5ba04c4777dcaaa5bc5b465b5eef48061b0f97114da11c0f23362f01fb51b284a1431e074158fb728945a1f62bf53321de432a09f'
