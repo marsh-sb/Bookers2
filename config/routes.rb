68869fd5abab79f3dd8eeecdd18b8d07e7a8119f
marsh-sb <enotaku5526@gmail.com>
2020-12-20 19:22:07 +0000

    [Add] フォロー・フォロワー機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,18 @@
 Rails.application.routes.draw do
  devise_for :users
- 
  root :to =>"homes#top"
  get "/home/about" => "homes#about"
+ 
+ resources :users
  
  resources :books do
   resources :book_comments, only: [:create, :destroy]
   resources :favorites, only: [:create, :destroy]
  end
  
- 
- resources :users, only: [:index,:show,:edit,:update]
- 
+ post 'follow/:id' => 'relationships#follow', as: 'follow' # フォローする
+ post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow' # フォロー外す
+ resources :users, only: :show
+ get 'users/:id/follower' => 'users#follower', as: 'user_follower'
+ get 'users/:id/followed' => 'users#followed', as: 'user_followed'
 end
