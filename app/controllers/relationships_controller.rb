75b4428c0c2f1d169e9fd8cefb45363a2d3eebee
marsh-sb <enotaku5526@gmail.com>
2020-12-21 09:44:16 +0000

    [Update]code fix

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,13 +1,22 @@
 class RelationshipsController < ApplicationController
+  before_action :authenticate_user!
+	def create
+		current_user.follow(params[:user_id])
+		redirect_to request.referer
+	end
 
-  def follow
-    current_user.follow(params[:id])
-    redirect_back(fallback_location: root_path)
+	def destroy
+		current_user.unfollow(params[:user_id])
+		redirect_to request.referer
+	end
+
+  def followings
+		user = User.find(params[:user_id])
+		@users = user.followings
   end
 
-  def unfollow
-    current_user.unfollow(params[:id])
-    redirect_back(fallback_location: root_path)
+  def followers
+		user = User.find(params[:user_id])
+		@users = user.followers
   end
-  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,17 @@
 Rails.application.routes.draw do
  devise_for :users
- root :to =>"homes#top"
- get "/home/about" => "homes#about"
- 
- resources :users
- 
- resources :books do
-  resources :book_comments, only: [:create, :destroy]
-  resources :favorites, only: [:create, :destroy]
- end
- 
- post 'follow/:id' => 'relationships#follow', as: 'follow' # フォローする
- post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow' # フォロー外す
- resources :users, only: :show
- get 'users/:id/follower' => 'users#follower', as: 'user_follower'
- get 'users/:id/followed' => 'users#followed', as: 'user_followed'
+  root :to => "homes#top"
+  get "home/about" => "homes#about"
+
+  resources :books, only: [:index, :show, :edit, :create, :destroy, :update] do
+    resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
+  end
+  
+  resources :users, only: [:index, :show, :edit, :update] do
+  	resource :relationships, only: [:create, :destroy]
+  	get 'followings' => 'relationships#followings', as: 'followings'
+  	get 'followers' => 'relationships#followers', as: 'followers'
+  end
+
 end
